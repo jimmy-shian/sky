67239ef3ea98f3773b0b1f8991c01d9d01f11422
jimmy-shian <jimmy910824@gmail.com>
2021-04-17 22:24:36 +0800

    icloud

--- mysite/views.py
+++ mysite/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 import random, datetime
 from mysite import models
 
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import smtplib
  
 def index(request):
@@ -24,63 +22,13 @@ def date(request):
 	#return HttpResponse("現在時刻 : {}".format(now))
 	return render(request, "date.html", locals())
 
-def mail(request):
-
-	return render(request, "mail.html", locals())
-
-def play(request):
-
-	return render(request, "play.html", locals())
-
-def play1(request):
-
-	return render(request, "play1.html", locals())
-
-def play2(request):
-
-	return render(request, "play2.html", locals())
-
-def play3(request):
-
-	return render(request, "play3.html", locals())
-
-def play4(request):
-
-	return render(request, "play4.html", locals())
-
-def play5(request):
-
-	return render(request, "play5.html", locals())
-
-def mom(request):
+def icloud(request):
 	
-	return render(request, "mom.html", locals())
+	return render(request, "icloud.html", locals())
 
 def index1(request):
 	
 	return render(request, "index1.html", locals())
 
-def index2(request):
-	
-	return render(request, "index2.html", locals())
 
-def index3(request):
-	
-	return render(request, "index3.html", locals())
-
-def passfirst(request):
-	
-	return render(request, "passfirst.html", locals())
-
-def passsecond(request):
-	
-	return render(request, "passsecond.html", locals())
-
-def passfourth(request):
-	
-	return render(request, "passfourth.html", locals())
-
-def passthird(request):
-	
-	return render(request, "passthird.html", locals())
 
--- sky/urls.py
+++ sky/urls.py
@@ -20,21 +20,10 @@ from mysite import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('date/', views.date),
-    path('mail/', views.mail),
     path('lotto/', views.lotto),
     path('', views.index),
-    path('index1/', views.index1),
-    path('index2/', views.index2),
-    path('index3/', views.index3),
-    path('mom/', views.mom),
-    path('passfirst/', views.passfirst),
-    path('passsecond/', views.passsecond),
-    path('passthird/', views.passthird),
-    path('passfourth/', views.passfourth),
-    path('play/', views.play ),
-    path('play1/', views.play1 ),
-    path('play2/', views.play2 ),
-    path('play3/', views.play3 ),
-    path('play4/', views.play4 ),
-    path('play5/', views.play5 )
+    path('icloud/', views.icloud),
+    path('index1/', views.index1)
+
+
 ]
