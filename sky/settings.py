cff3bec1cb9567fd68926daff8a901fa2fe14fd7
jimmy-shian <jimmy910824@gmail.com>
2021-04-21 19:29:14 +0800

    icloud

--- sky/settings.py
+++ sky/settings.py
@@ -121,13 +121,5 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-
-
-STATIC_URL = '/static/'
 STATIC_ROOT = '/staticfiles'
 django_heroku.settings(locals())
